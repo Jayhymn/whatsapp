3e53a192e8c056e6b4aa1de3ef005241cb65531c
Adetunji <adetunji.adegbite1@gmail.com>
2025-01-23 16:13:37 +0100

    created tab layout

---
+++ app/src/main/java/com/wakeupdev/whatsapp/WhatsAppTab.kt
@@ -0,0 +1,58 @@
+package com.wakeupdev.whatsapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.wakeupdev.whatsapp.ui.theme.WhatsAppTheme
+import kotlinx.coroutines.launch
+
+@Composable
+fun WhatsAppTab(modifier: Modifier = Modifier) {
+    val tabs = listOf("Chats", "Status", "Calls")
+
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        pageCount = { tabs.size }
+    )
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text(title) },
+                    selected = pagerState.currentPage == index,
+                    onClick = { coroutineScope.launch { pagerState.animateScrollToPage(index) } }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier,
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WhatsAppTabPrev() {
+    WhatsAppTheme {
+        WhatsAppTab()
+    }
+}
--- app/src/main/java/com/wakeupdev/whatsapp/components/AuthButton.kt
+++ app/src/main/java/com/wakeupdev/whatsapp/components/AuthButton.kt
@@ -2,12 +2,15 @@ package com.wakeupdev.whatsapp.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.wakeupdev.whatsapp.ui.theme.WhatsAppGreen
 import com.wakeupdev.whatsapp.ui.theme.WhatsAppTheme
 
 @Composable
@@ -17,14 +20,19 @@ fun AuthButton(
     ) {
     FilledTonalButton(
         modifier = Modifier.fillMaxWidth(),
+        colors = ButtonColors(
+            contentColor = Color.White,
+            containerColor = WhatsAppGreen,
+            disabledContainerColor = Color.Gray,
+            disabledContentColor = Color.White
+        ),
         onClick = {}
     ) {
         Text(text, style = MaterialTheme.typography.bodyLarge,)
     }
 }
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview
 @Composable
 private fun AuthButtonPrev() {
     WhatsAppTheme {
